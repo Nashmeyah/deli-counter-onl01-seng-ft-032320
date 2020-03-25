1025c3f0d2fb6068344a01c477f179a8f5d59360
Nashmeyah Al-Rekabi <nashalrekabi@gmail.com>
2020-03-25 21:23:20 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,11 +4,12 @@ def line(katz_deli)
   if katz_deli.length > 0
     line_number = 1
     katz_deli.each do |name|
-    return "The line is currently: #{line_number}. #{name}"
-    line_number +=1
+      return "The line is currently: #{line_number}. #{name}"
+      line_number +=1
     end
-  elsif
+  else
     "The line is currently empty."
+    end
 end
 
 def take_a_number(katz_deli, name)
